2fcd3fc89968b311463b19a848d149f94bc59a21
Toño <caaz160782@gmail.com>
2025-05-26 20:39:44 -0600

    end funciones

---
+++ SumarConModulo.py
@@ -0,0 +1,12 @@
+#import moduloSumar
+from moduloSumar import sumar
+
+print('*** Función sumar ***')
+
+
+# Llamar a la funcion
+resultado_funcion = sumar(8, 5)
+print(f'Resultado función sumar: {resultado_funcion}')
+
+resultado_funcion = sumar(9, 15)
+print(f'Resultado función sumar: {resultado_funcion}')
---
+++ moduloSumar.py
@@ -0,0 +1,4 @@
+# Definimos la funcion
+def sumar(a, b):
+    resultado_suma = a + b
+    return resultado_suma
